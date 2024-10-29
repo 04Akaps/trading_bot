b2b343f426d4b2f4407cdb4152ea7acef45fef17
jukas <jukas@wemade.com>
2024-10-29 17:10:32 +0900

    Feat: slack noti client

--- client/slack/root.go
+++ client/slack/root.go
@@ -1,1 +1,58 @@
 package slack
+
+import (
+	"context"
+	"github.com/04Akaps/trading_bot.git/config"
+	"github.com/slack-go/slack"
+	"log"
+	"os"
+)
+
+type SlackClient struct {
+	client *slack.Client
+	id     string
+}
+
+func NewSlackClient(cfg config.Slack) SlackClient {
+	var client SlackClient
+
+	client.client = slack.New(
+		cfg.Token,
+		slack.OptionDebug(true),
+		slack.OptionLog(log.New(os.Stdout, "slack:api: ", log.Lshortfile|log.LstdFlags)),
+	)
+	client.id = cfg.ChannelID
+
+	return client
+}
+
+//
+//attachment := slack.Attachment{
+//Pretext: "some pretext",
+//Text:    "some text",
+//// Uncomment the following part to send a field too
+///*
+//	Fields: []slack.AttachmentField{
+//		slack.AttachmentField{
+//			Title: "a",
+//			Value: "no",
+//		},
+//	},
+//*/
+//}
+
+//slack.MsgOptionAttachments(attachment),
+
+func (c SlackClient) TestMessage(format string) {
+	_, _, err := c.client.PostMessageContext(
+		context.Background(),
+		c.id,
+		slack.MsgOptionText(format, false),
+
+		slack.MsgOptionAsUser(true),
+	)
+
+	if err != nil {
+		// TODO logger
+	}
+}
--- go.mod
+++ go.mod
@@ -14,7 +14,9 @@ require (
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/go-resty/resty/v2 v2.15.3 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/slack-go/slack v0.15.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/net v0.27.0 // indirect
